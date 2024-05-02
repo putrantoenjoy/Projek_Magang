074db36f4a63249256ba99d0a2a6155509c68d00
bagus ika putranto <33688117+putrantoenjoy@users.noreply.github.com>
2024-05-02 14:53:31 +0700

    done ui

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -26,14 +26,32 @@
     <div id="root" class="root mn--max hd--expanded">
         <!-- CONTENTS -->
         <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
-        <section id="content" class="content">
+        <section id="content" class="content" style="background-color: #e9e9e9">
+            <div class="position-absolute" style="background-color: #0c58ca; height: 150px; width: 100%"></div>
             {{-- @yield('content') --}}
             <div class="container">
                 <div class="d-flex flex-column m-3">
-                    <p>User</p>
-                    <h3>Data User</h3>
+                    <div class="card bg-transparent">
+                        <p class="text-white fs-5">User</p>
+                        <h3 class="text-white" class="fw-bold">Data User</h3>
+                    </div>
                     <div class="card">
-                        <table class="table p-3">
+                        <div class="d-flex flex-column px-3 pt-3">
+                            <div class="border-bottom  pb-3">
+                                <h4>Tabel User</h4>
+                                <div class="d-flex justify-content-between">
+                                    <div class="d-flex">
+                                        <button class="btn btn-primary">Menambah User</button>
+                                        <button class="btn btn-success mx-3">Export</button>
+                                    </div>
+                                    <div class="d-flex">
+                                        <input type="text" class="form-control mx-3" placeholder="cari">
+                                        <button class="btn btn-primary">Cari</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <table class="table table-striped px-3">
                             <thead class="fw-bold">
                                 <td>User Id</td>
                                 <td>Nama</td>
@@ -43,6 +61,28 @@
                                 <td>Aksi</td>
                             </thead>
                             <tbody>
+                                <tr>
+                                    <td>U00001</td>
+                                    <td>khosim</td>
+                                    <td>admin</td>
+                                    <td>khosim@gmail.com</td>
+                                    <td>admin</td>
+                                    <td>
+                                        <a href="" class="btn btn-primary">Ubah</a>
+                                        <a href="" class="btn btn-danger">Hapus</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>U00001</td>
+                                    <td>khosim</td>
+                                    <td>admin</td>
+                                    <td>khosim@gmail.com</td>
+                                    <td>admin</td>
+                                    <td>
+                                        <a href="" class="btn btn-primary">Ubah</a>
+                                        <a href="" class="btn btn-danger">Hapus</a>
+                                    </td>
+                                </tr>
                                 <tr>
                                     <td>U00001</td>
                                     <td>khosim</td>
